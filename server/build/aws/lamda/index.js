276d66ca5181f7747e48ee92e45be8426af545bf
Gustavo Blasius <54649777+zBlasius@users.noreply.github.com>
2024-06-11 11:48:40 -0300

    fix: remove ./build

--- server/build/aws/lamda/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// * OBJETIVO DE PROJETO
-// * Preciso aprender sobre as seguintes ferramentas: 
-// * Mysql + Prisma
-// * AWS - s3
-// * AWS - lambda
-// * AWS - sqs
-// * Utilizar esse projeto como ponto de partida 
