104068439a86af9451021b678f6675ec8941c996
Gustavo Blasius <54649777+zBlasius@users.noreply.github.com>
2024-07-30 22:12:27 -0300

    feat: add jsdoc

--- server/src/modules/user/domain/service/contracts/IUserService.ts
+++ server/src/modules/user/domain/service/contracts/IUserService.ts
@@ -5,28 +5,39 @@ import { LoginInfo, RegisterInfo } from "../../../types";
  * Interface for user services.
  */
 export interface IUserService {
+  /**
+   * Logs in a user.
+   *
+   * @param {LoginInfo} param0 - Login information.
+   * @param {string} param0.username - Username.
+   * @param {string} param0.password - User password.
+   * @returns {Promise<any>} A promise that resolves when the login is successful.
+   */
+  login({ username, password }: LoginInfo): Promise<string>;
 
-    /**
-     * Logs in a user.
-     * 
-     * @param {LoginInfo} param0 - Login information.
-     * @param {string} param0.username - Username.
-     * @param {string} param0.password - User password.
-     * @returns {Promise<any>} A promise that resolves when the login is successful.
-     */
-    login({username, password}: LoginInfo): Promise<string>; 
+  /**
+   * Registers a new user.
+   *
+   * @param {RegisterInfo} param - Registration information.
+   * @param {string} param.name - Full name of the user.
+   * @param {string} param.username - Username.
+   * @param {string} param.email - User email.
+   * @param {string} param.type - User type.
+   * @returns {Promise<void>} A promise that resolves when the registration is successful.
+   */
+  register({
+    name,
+    username,
+    email,
+    type,
+    password,
+  }: RegisterInfo): Promise<void>;
 
-    /**
-     * Registers a new user.
-     * 
-     * @param {RegisterInfo} param - Registration information.
-     * @param {string} param.name - Full name of the user.
-     * @param {string} param.username - Username.
-     * @param {string} param.email - User email.
-     * @param {string} param.type - User type.
-     * @returns {Promise<void>} A promise that resolves when the registration is successful.
-     */
-    register({name, username, email, type, password}: RegisterInfo): Promise<void>;
-
-    authenticate(token:string | undefined):Promise<string>;
+  /**
+   * Authenticates a user based on the provided token.
+   *
+   * @param {string | undefined} token - The authentication token. If undefined, authentication will fail.
+   * @returns {Promise<string>} A promise that resolves to a string indicating the authentication result.
+   */
+  authenticate(token: string | undefined): Promise<string>;
 }
