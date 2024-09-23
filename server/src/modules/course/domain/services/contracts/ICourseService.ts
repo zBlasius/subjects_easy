38745ff710cca9a14f68d3281de805cd2fe4fd74
Gustavo Blasius <54649777+zBlasius@users.noreply.github.com>
2024-09-23 14:12:56 -0300

    feat: Icourseservice jsdoc

--- server/src/modules/course/domain/services/contracts/ICourseService.ts
+++ server/src/modules/course/domain/services/contracts/ICourseService.ts
@@ -1,31 +1,48 @@
 import CourseModel from "../../model/CourseModel";
 
-// TODO - Fazer funcionar
+/**
+ * Interface that defines the service layer for Course operations.
+ * Provides methods to create, list, retrieve, and get details of courses.
+ */
 export interface ICourseService {
   /**
-   *
-   * Should create
-   *
+   * Creates a new course associated with a specific user.
+   * 
+   * @param {Object} data - The data required to create a course.
+   * @param {string} data.userId - The ID of the user creating the course.
+   * @param {string} data.title - The title of the course.
+   * @param {string} data.description - A brief description of the course.
+   * @returns {Promise<void>} A promise that resolves when the course is successfully created.
    */
   create(data: {
     userId: string;
     title: string;
     description: string;
-  }): Promise<void>; //! Make better
+  }): Promise<void>;
 
   /**
-   *
-   * Should list
-   *
+   * Retrieves a list of courses associated with a specific user's email.
+   * 
+   * @param {string} email - The email of the user to fetch the courses for.
+   * @returns {Promise<CourseModel[] | undefined>} A promise that resolves to an array of CourseModel instances
+   * or undefined if no courses are found.
    */
   listByUser(email: string): Promise<CourseModel[] | undefined>;
 
   /**
-   *
-   * Should get by id
-   *
+   * Fetches a course by its ID.
+   * 
+   * @param {string} id - The unique identifier of the course.
+   * @returns {Promise<CourseModel | null>} A promise that resolves to the CourseModel instance if found, 
+   * or null if no course with the specified ID exists.
    */
   getById(id: string): Promise<CourseModel | null>;
 
+  /**
+   * Retrieves the details of a specific course by its ID.
+   * 
+   * @param {string} courseId - The unique identifier of the course.
+   * @returns {Promise<CourseModel>} A promise that resolves to the CourseModel instance with all its details.
+   */
   getDetails(courseId: string): Promise<CourseModel>;
 }
